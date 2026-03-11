186a76e9c86355a147f89875875e036361451403
Kahero Harriott <kaheroharriott@gmail.com>
2026-03-10 19:50:57 -0500

    Some initial testing

---
+++ src/learning/mod.rs
@@ -0,0 +1,47 @@
+use std::sync::Arc;
+#[cfg(target_arch = "wasm32")]
+use winit::event_loop;
+use winit::{
+    application::ApplicationHandler,
+    event::*,
+    event_loop::{ActiveEventLoop, EventLoop},
+    keyboard::{KeyCode, PhysicalKey},
+    window::Window,
+};
+
+#[cfg(target_arch = "wasm32")]
+use wasm_bindgen::prelude::*;
+
+pub struct GameState {
+    window: Arc<Window>,
+}
+
+impl GameState {
+    pub async fn new(window: Arc<Window>) -> anyhow::Result<Self> {
+        Ok(Self { window })
+    }
+
+    pub fn resize(&mut self, _width: u32, _height: u32) {}
+
+    pub fn render(&mut self) {
+        self.window.request_redraw();
+    }
+}
+
+pub struct App {
+    #[cfg(target_arch = "wasm32")]
+    proxy: Option<winit::event_loop::EventLoopProxy<GameState>>,
+    state: Option<GameState>,
+}
+
+impl App {
+    pub fn new(#[cfg(target_arch = "wasm32")] event_loop: &EventLoop<GameState>) -> Self {
+        #[cfg(target_arch = "wasm32")]
+        let proxy = Some(event_loop.create_proxy());
+        Self {
+            state: None,
+            #[cfg(target_arch = "wasm32")]
+            proxy,
+        }
+    }
+}
